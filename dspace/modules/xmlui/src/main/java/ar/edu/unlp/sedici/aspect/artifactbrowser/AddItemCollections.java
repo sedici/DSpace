8e840b35da550d2219deaccda28d74e5c0383521
Ariel Lira <arieljlira@gmail.com>
2014-07-01 12:03:15 -0300

    mas y mas merge

--- dspace/modules/xmlui/src/main/java/ar/edu/unlp/sedici/aspect/artifactbrowser/AddItemCollections.java
+++ dspace/modules/xmlui/src/main/java/ar/edu/unlp/sedici/aspect/artifactbrowser/AddItemCollections.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.dspace.app.xmlui.aspect.artifactbrowser;
+package ar.edu.unlp.sedici.aspect.artifactbrowser;
 
 import java.io.IOException;
 import java.io.Serializable;
