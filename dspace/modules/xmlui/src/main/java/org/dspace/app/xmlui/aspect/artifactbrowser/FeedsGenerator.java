c603d80bc427ad1d37ed5233ca62bcc253280cb5
Ariel Lira <arieljlira@gmail.com>
2014-07-01 11:20:44 -0300

    mas merges

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/FeedsGenerator.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/FeedsGenerator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ar.edu.unlp.sedici.aspect.artifactbrowser;
+package org.dspace.app.xmlui.aspect.artifactbrowser;
 
 import java.io.IOException;
 import java.io.Serializable;
