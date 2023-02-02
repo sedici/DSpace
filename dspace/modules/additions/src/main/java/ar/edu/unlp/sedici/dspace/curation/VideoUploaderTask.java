362c3b329bb4752ca53b267b633f53fd3357f7a0
Pablo de Albuquerque <pablo.cda@gmail.com>
2023-02-02 11:04:08 -0300

    8557 Agrego VideoUploaderTask.java vacio

---
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/curation/VideoUploaderTask.java
@@ -0,0 +1,24 @@
+package ar.edu.unlp.sedici.dspace.curation;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.dspace.content.DSpaceObject;
+import org.dspace.content.Item;
+import org.dspace.content.Metadatum;
+import org.dspace.content.authority.Choices;
+import org.dspace.core.Constants;
+import org.dspace.curate.AbstractCurationTask;
+import org.dspace.curate.Curator;
+
+public class VideoUploaderTask extends AbstractCurationTask {
+
+	@Override
+	public int perform(DSpaceObject dso) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
