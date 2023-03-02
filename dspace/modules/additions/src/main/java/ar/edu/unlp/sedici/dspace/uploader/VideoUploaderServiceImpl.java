345828ee00bedf5639a08bfa65c7e7a05774736e
Rafael Bertoli <bertolirafa@gmail.com>
2023-03-02 09:40:24 -0300

    8557-Construccion de descipcion

--- dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/uploader/VideoUploaderServiceImpl.java
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/uploader/VideoUploaderServiceImpl.java
@@ -11,9 +11,11 @@ import org.apache.log4j.Logger;
 import org.dspace.content.Bitstream;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
+import org.dspace.content.Metadatum;
 import org.dspace.core.Context;
 import org.dspace.curate.Curator;
 import org.dspace.handle.HandleManager;
+import org.jsoup.Jsoup;
 import org.springframework.stereotype.Service;
 import ar.edu.unlp.sedici.dspace.google.YoutubeAdapter;
 
@@ -29,8 +31,35 @@ public class VideoUploaderServiceImpl implements ContentUploaderService{
 	@Override
 	public void uploadContent(Item item) throws Throwable {
         String handle = item.getHandle();
+        log.info("Upload del item " + handle +" a YouTube");
         String title= item.getMetadata("dc.title");//Falta determinar que hacer is hay muchos videos, como se construye el titulo
-        String description = item.getMetadata("dc. description. abstract");//falta derterminar que mas se agrega a la descripcion
+        System.out.println(title);
+        String description = item.getMetadata("dc.title")+"\n";//falta derterminar que mas se agrega a la descripcion
+        List<Metadatum> creators = item.getMetadata("sedici","creator","person",Item.ANY,Item.ANY);
+        Integer auxNumerico = 1;
+        if (creators.size() > 1) {
+        	description = description+"Creadores: "+creators.get(0).value;
+        	while(auxNumerico<creators.size()) {
+        		description = description+"; "+creators.get(auxNumerico).value;
+        		auxNumerico = auxNumerico + 1;
+        	}
+        	description= description+"\n";
+        }else {
+        	description = description+"Creador: "+creators.get(0).value+"\n";
+        }
+        description = description+"Fecha de publicación: "+item.getMetadata("dc.date.available")+"\n";
+        description = description+"Enlace de la fuente: "+item.getMetadata("dc.identifier.uri")+"\n";
+        List<Metadatum> subjects = item.getMetadata("dc","subject",Item.ANY,Item.ANY,Item.ANY);
+        description = description+"Palabras clave: ";
+        auxNumerico = 0;
+        while (auxNumerico<(subjects.size()-1)) {
+        	description = description+subjects.get(auxNumerico).value+", ";
+        	auxNumerico = auxNumerico +1;
+        }
+        description = description+subjects.get(auxNumerico).value+"\n";
+        description = description+"Resumen: "+Jsoup.parse(item.getMetadata("dc.description.abstract")).text()+"\n";
+        description = description+"Licencia de uso: "+item.getMetadata("sedici.rights.license")+"\n";
+        System.out.println(description);
         List<String> tags = new ArrayList<String>();
         tags.add("prueba");//Definir que poner en los tags
         Bitstream[] bitstreams = item.getBundles("ORIGINAL")[0].getBitstreams();
@@ -41,8 +70,8 @@ public class VideoUploaderServiceImpl implements ContentUploaderService{
         			String videoID = new YoutubeAdapter().uploadVideo(bitstream.retrieve(), title, description, tags);
             		log.info("Se subio el video con id "+videoID);
             		String schema = "sedici";
-            		String element = "video";
-            		String qualifier = "videoId";
+            		String element = "indentifier";
+            		String qualifier = "youtubeId";
             		String lang = null;
             		bitstream.addMetadata(schema,element,qualifier,lang,videoID);
             		bitstream.updateMetadata();
@@ -52,8 +81,6 @@ public class VideoUploaderServiceImpl implements ContentUploaderService{
         		
         	}
         }
-        
-		log.info("Upload del item " + handle +" a YouTube");
 		
 	}
 
